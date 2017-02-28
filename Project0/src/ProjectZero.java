49e7f0e2ec3f97fde2f076a49ae505c1e58847cc
Stephany B <Stephany B@DESKTOP-PM2LMVE>
2017-02-27 23:01:46 -0500

    Started the binary integer input exception (alternative)<!COMPLETE>

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -2,12 +2,31 @@ import java.util.Scanner;
 
 public class ProjectZero {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+{
 
 		Scanner cin = new Scanner(System.in);
 		System.out.print("Enter two numbers: ");
 
-		if (cin.hasNextInt()) {
+		if(cin.hasNextInt(2))
+		{
+			int first = cin.nextInt();//reads in integer 1001
+			String binNum = Integer.toString(first); //converts int read in to a string 1001 to "1001"
+			//1001 - converts number to base 10 ex/1001001 = 1001 and returns that string not "1001
+			//corrected: using incorrect base, should be using base 2 not base 10 as previously used
+			
+			int convBinNum = Integer.parseInt(binNum,2);//reads 1001 and converts it to 9 in base 10
+			
+			if (cin.hasNextInt()) 
+			{
+				int second = cin.nextInt();
+				bigFunction(convBinNum, second);
+			}
+		}
+		
+		/*
+			
+		else if (cin.hasNextInt()) {
 			int first = cin.nextInt();
 			
 			if (cin.hasNextInt()) {
@@ -35,8 +54,10 @@ public class ProjectZero {
 			System.out.println("Please try again and enter numbers.");
 		}
 
-		cin.close();	
-	}
+			*/
+		cin.close();
+		
+}
 	public static void bigFunction(int a, int b) {
 		int sum = a+b;
 		System.out.println(" The sum of " + a + " + "+ b + " = "+sum);
